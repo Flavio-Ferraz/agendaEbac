332bc3af818c55522ed0714246a990e496fa8236
Flavio de Oliveira Ferraz <70780820+Flavio-Ferraz@users.noreply.github.com>
2023-05-01 11:54:03 -0300

    fix reorganização do código em funções

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 const form = document.getElementById('form-inserir-dados');
+const telCadastrado =[];
 let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    adicionaLinha();
+    atualizaTabela();
+});
 
+function adicionaLinha(){
     const inputNomeContato = document.getElementById('nome-contato');
     const inputDddContato = document.getElementById('ddd-contato');
     const inputTelContato = document.getElementById('telefone-contato');
 
-    let linha = '<tr>';
-    linha +=`<td>${inputNomeContato.value}</td>`;
-    linha += `<td>${inputDddContato.value}</td>`;
-    linha += `<td>${inputTelContato.value}</td>`;
-    linha+= '</tr>';
-
-    linhas += linha;
-
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
+        let linha = '<tr>';
+        linha +=`<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputDddContato.value}</td>`;
+        linha += `<td>${inputTelContato.value}</td>`;
+        linha+= '</tr>';
+    
+        linhas += linha;
 
     inputNomeContato.value = '';
     inputDddContato.value = '';
     inputTelContato.value = '';
-});
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
